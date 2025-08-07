c5c250e6217e80590b36f086b6cf314541abca68
Mauricio Gonzalez Valero <mau@Mauricios-MacBook-Air.local>
2025-08-07 12:44:25 -0600

    thu 7 ago

--- PrimerParcial/EjerciciosClase/ContarVocalesString.cpp
+++ PrimerParcial/EjerciciosClase/ContarVocalesString.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctype.h>
 
 using namespace std;
 
@@ -8,8 +9,8 @@ int main(){
     int vocales=0;
     cout<<"Dame una palabra"<<endl;
     cin>>palabra;
-
     for(int i=0;i<palabra.size();i++){
+        palabra[i]=tolower(palabra[i]);
         if(palabra[i]=='a' || palabra[i]=='e' || palabra[i]=='i' || palabra[i]=='o' || palabra[i]=='u' ){
             vocales++;
         }
