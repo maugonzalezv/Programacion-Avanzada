84f5f3eedeaf487d68cea09d52db49bc2516d144
Mauricio Gonzalez Valero <mau@Mauricios-MacBook-Air.local>
2025-08-25 11:58:01 -0600

    mon25aug

--- PrimerParcial/tareamon25aug/noregex.cpp
+++ PrimerParcial/tareamon25aug/noregex.cpp
@@ -11,7 +11,12 @@ int main(){
     string email;
     cout<<"Ingrese email: ";
     cin>>email;
-    
+
+    if(validar(email)){
+        cout<<"Email valido"<<endl;
+    } else {
+        cout<<"Email invalido"<<endl;
+    }   
     
     return 0;
 }
