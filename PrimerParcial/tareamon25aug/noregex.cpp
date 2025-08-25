e582641e30531239b89757894470bc853240ec78
Mauricio Gonzalez Valero <mau@Mauricios-MacBook-Air.local>
2025-08-25 11:51:09 -0600

    mon25aug

--- PrimerParcial/tareamon25aug/noregex.cpp
+++ PrimerParcial/tareamon25aug/noregex.cpp
@@ -3,34 +3,11 @@
 
 using namespace std;
 
+bool validar(string email){
+    
+}
+
 int main(){
-    int ancho = 65;
-    string palabra;
-    string linea[200];
-    int n = 0;
-    int sumaLetras = 0;
-
-    while(cin>>palabra){
-        int len = (int)palabra.size();
-        if(n==0){
-            linea[0]=palabra;
-            n=1;
-            sumaLetras=len;
-        } else {
-            int largoConNueva = sumaLetras + n + len;
-            if(largoConNueva<=ancho){
-                linea[n]=palabra;
-                n=n+1;
-                sumaLetras=sumaLetras+len;
-            } else {
-                imprimirLinea(linea, n, sumaLetras, 0, ancho);
-                linea[0]=palabra;
-                n=1;
-                sumaLetras=len;
-            }
-        }
-    }
-
-    imprimirLinea(linea, n, sumaLetras, 1, ancho);
+    
     return 0;
 }
