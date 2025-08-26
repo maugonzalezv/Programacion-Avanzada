4d9f611e1823a794bf70546f8f8754bbecc65736
Mauricio Gonzalez Valero <mau@Mauricios-MacBook-Air.local>
2025-08-25 23:48:29 -0600

    mon25aug

--- PrimerParcial/tareamon25aug/noregex.cpp
+++ PrimerParcial/tareamon25aug/noregex.cpp
@@ -1,36 +1,57 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <cctype>
-
 using namespace std;
 
 bool validar(string email){
-    int atCount = 0;
-    int atPosition = -1;
+    if(email.size() == 0){
+        throw "No ingresaste ningun valor";
+    }
 
-    transform(email.begin(), email.end(), email.begin(), :: tolower);
+    for(int i=0; i<email.size(); i++){
+        if(email[i] >= 'A' && email[i] <= 'Z'){
+            email[i] = email[i] - 'A' + 'a';
+        }
+    }
 
     char final = email[email.size()-1];
-    if(final> 'a' || final <'z'){
+    if(final < 'a' || final > 'z'){
         return false;
     }
-
-    for(int i=0;i<email.size();i++){
-        if(email[i] == '.'){
-            if(email[i+1]=='.'){
-                return false;
-            }
-            if(i==0 || i==email.size()-1){
-                return false;
-            }
-        }
-        
-        if(email)
-
+    if(email[email.size()-1] == '.'){
+        return false;
     }
+    if(email[0] == '.') return false;
+    for(int i=0; i<email.size()-1; i++){
+        if(email[i] == '.' && email[i+1] == '.'){
+            return false;
+        }
+    }
+
+    int atCount = 0;
+    int atPos = -1;
+    for(int i=0; i<email.size(); i++){
+        if(email[i] == '@'){
+            atCount++;
+            atPos = i;
+            if(atCount > 1) return false;
+        }
+    }
+
+    if(atCount != 1) return false;
+    if(atPos == 0 || atPos == email.size()-1) return false;
+    if(email[atPos-1] == '.') return false;
+    if(email[atPos+1] == '.') return false;
+
+    bool puntoDespues = false;
+    for(int i=atPos+1; i<email.size(); i++){
+        if(email[i] == '.'){
+            puntoDespues = true;
+            break;
+        }
+    }
+    if(puntoDespues == false) return false;
+
     return true;
-    
 }
 
 int main(){
@@ -38,11 +59,15 @@ int main(){
     cout<<"Ingrese email: ";
     cin>>email;
 
-    if(validar(email)){
-        cout<<"Email valido"<<endl;
-    } else {
-        cout<<"Email invalido"<<endl;
-    }   
+    try{
+        if(validar(email)){
+            cout<<"Email valido"<<endl;
+        } else {
+            cout<<"Email invalido"<<endl;
+        }
+    } catch(...){
+        cout<<"Ocurrio un error"<<endl;
+    }
 
     return 0;
 }
