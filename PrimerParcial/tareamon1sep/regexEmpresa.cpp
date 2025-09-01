d271d7625b02dfbc14879f85919adad96db60ac7
maugonzalezv <maugzzv2404@gmail.com>
2025-09-01 11:49:51 -0600

    mon1sep

---
+++ PrimerParcial/tareamon1sep/regexEmpresa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main(){
+    string texto;
+    string empresa = "";
+    string correo = "";
+    string cp = "";
+    
+}
