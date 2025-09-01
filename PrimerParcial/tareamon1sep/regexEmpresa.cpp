d93c4660c4ceae7bb21bcf312e756e3e453adea5
maugonzalezv <maugzzv2404@gmail.com>
2025-09-01 12:03:13 -0600

    mon1sep

--- PrimerParcial/tareamon1sep/regexEmpresa.cpp
+++ PrimerParcial/tareamon1sep/regexEmpresa.cpp
@@ -9,5 +9,21 @@ int main(){
     string empresa = "";
     string correo = "";
     string cp = "";
-    
+
+    cout<<"Inserte el texto: "<<endl;
+    getline(cin, texto);
+
+    try { 
+        if(texto.size()==0){
+            throw "No hay texto";
+        }
+
+        regex rEmpresa("empresa\\s*\"([^\"]+)\"");
+        regex rCorreo("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,63}");
+        regex rCP("CP\\s*(\\d{5})");
+
+    } catch (...) {
+
+    }
+
 }
