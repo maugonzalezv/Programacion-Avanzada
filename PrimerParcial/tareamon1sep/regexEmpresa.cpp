8373090d5fef48077da76177cf9c1287bede1d18
maugonzalezv <maugzzv2404@gmail.com>
2025-09-01 12:14:15 -0600

    mon1sep

--- PrimerParcial/tareamon1sep/regexEmpresa.cpp
+++ PrimerParcial/tareamon1sep/regexEmpresa.cpp
@@ -22,8 +22,25 @@ int main(){
         regex rCorreo("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,63}");
         regex rCP("CP\\s*(\\d{5})");
 
-    } catch (...) {
+        for(sregex_iterator it(texto.begin(), texto.end(), rEmpresa), end; it!=end; ++it){
+            empresa = (*it)[1];
+        }
 
+        for(sregex_iterator it(texto.begin(), texto.end(), rCorreo), end; it!=end; ++it){
+            correo = (*it)[0];
+        }
+
+        for(sregex_iterator it(texto.begin(), texto.end(), rCP), end; it!=end; ++it){
+            cp = (*it)[1];
+        }
+
+        cout << "Empresa: " << empresa << endl;
+        cout << "Correo: " << correo << endl;
+        cout << "CP: " << cp << endl;
+
+    } catch (...) {
+        cout << "Ocurrio un error" << endl;
     }
 
+    return 0;
 }
