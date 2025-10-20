2028f7ab4df00a6a9d0650dda79dc98c4d5e4bf6
maugonzalezv <maugzzv2404@gmail.com>
2025-10-20 12:24:17 -0600

    mon20oct

--- M594595P2EJ2.cpp
+++ M594595P2EJ2.cpp
@@ -27,42 +27,41 @@ void calcularDP(int a[5][5], int dp[5][5], int px[5][5], int py[5][5]) {
     }
 }
 
-void imprimirRuta(int a[5][5], int px[5][5], int py[5][5]) {
+int reconstruirRuta(int a[5][5], int px[5][5], int py[5][5], int rutaVals[25], int rutaI[25], int rutaJ[25]) {
     int i = 4, j = 4;
-    int rutaI[25], rutaJ[25], rutaVal[25];
+    int tmpVals[25], tmpI[25], tmpJ[25];
     int k = 0;
 
     while (i != -1 && j != -1) {
-        rutaI[k] = i;
-        rutaJ[k] = j;
-        rutaVal[k] = a[i][j];
-        int ni = px[i][j];
-        int nj = py[i][j];
+        tmpVals[k] = a[i][j];
+        tmpI[k] = i;
+        tmpJ[k] = j;
+        int ni = px[i][j], nj = py[i][j];
         i = ni; j = nj;
         k++;
     }
 
-    cout << "Ruta:" << endl;
-    for (int t = k-1; t >= 0; t--) {
-        cout << "(" << rutaI[t] << "," << rutaJ[t] << ") val=" << rutaVal[t];
-        if (t) cout << " -> ";
+    // invertir
+    int n = k;
+    for (int t = 0; t < n; t++) {
+        rutaVals[t] = tmpVals[n - 1 - t];
+        rutaI[t] = tmpI[n - 1 - t];
+        rutaJ[t] = tmpJ[n - 1 - t];
     }
-    cout << endl;
+    return n;
 }
 
 int main() {
     try {
         int a[5][5] = {
             {80, 200, 50, 30, 5},
-            {40, 25, 10, 20, 30},
-            {67, 110, 20, 200, 100},
-            {80, 6, 45, 3, 12},
-            {24, 7, 34, 15, 10}
+            {40,  25, 10, 20, 30},
+            {67, 110, 20, 200,100},
+            {80,   6, 45,  3, 12},
+            {24,   7, 34, 15, 10}
         };
 
-        int dp[5][5];
-        int px[5][5], py[5][5];
-
+        int dp[5][5], px[5][5], py[5][5];
         calcularDP(a, dp, px, py);
 
         cout << "Debug DP max suma por celda:" << endl;
@@ -73,9 +72,37 @@ int main() {
             }
             cout << endl;
         }
+        cout << "Maxima suma (inicio->fin): " << dp[4][4] << endl;
 
-        cout << "Maxima suma: " << dp[4][4] << endl;
-        imprimirRuta(a, px, py);
+        int rutaVals[25], rutaI[25], rutaJ[25];
+        int n = reconstruirRuta(a, px, py, rutaVals, rutaI, rutaJ);
+
+        cout << "Ruta (valores): ";
+        for (int t = 0; t < n; t++) {
+            cout << rutaVals[t];
+            if (t < n-1) cout << " -> ";
+        }
+        cout << endl;
+
+        // Mejor bloque contiguo de 3 en la secuencia (yo + 2 siguientes) o (yo + 2 anteriores)
+        int bestSuma = -1, bestIdx = -1; // bestIdx = indice inicial del bloque de 3
+        for (int t = 0; t + 2 < n; t++) {
+            int s = rutaVals[t] + rutaVals[t+1] + rutaVals[t+2];
+            if (s > bestSuma) {
+                bestSuma = s;
+                bestIdx = t;
+            }
+        }
+
+        if (bestIdx != -1) {
+            cout << "Mejor bloque de 3 contiguos en la ruta: "
+                 << rutaVals[bestIdx] << ", "
+                 << rutaVals[bestIdx+1] << ", "
+                 << rutaVals[bestIdx+2] << endl;
+            cout << "Suma = " << bestSuma << endl;
+        } else {
+            cout << "No hay bloque de 3." << endl;
+        }
     }
     catch (...) {
         cout << "Ocurrio un error" << endl;
